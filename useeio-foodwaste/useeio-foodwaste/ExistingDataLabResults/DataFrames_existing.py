4371bacf44351d682a654e62129afd687bade75d
Daniel L. Young, Ph.D <youngd62@outlook.com>
2020-06-04 11:49:22 -0400

    Replaced existing repository with new code base.

---
+++ useeio-foodwaste/useeio-foodwaste/ExistingDataLabResults/DataFrames_existing.py
@@ -0,0 +1,35 @@
+# DataFrames_existing.py (ExistingDataLabResults)
+# !/usr/bin/env python3
+# coding=utf-8
+# young.daniel@epa.gov
+
+
+"""
+Basic program to view lab results from .csv file.
+
+DataFrames = data in a frame/table or tabular data.
+"""
+
+first_run = false
+# NOTE: These lines need to be run on the first time only:
+if first_run
+    using Pkg
+    Pkg.add("DataFrames")
+    Pkg.add("CSV")
+end
+
+using DataFrames
+using CSV
+
+# The big csv takes a while to load/print, so I created one with ~10 lines
+input = ".\\TestFolderJulia\\sampledata.csv"
+input_smaller = ".\\TestFolderJulia\\sampledata_smaller.csv"
+
+df = DataFrame(CSV.File(input_smaller))
+
+# describe(df)
+print(df)
+
+# For all users
+# New variable: JULIA_HOME C:\Users\Daniel L. Young PhD\AppData\Local\Programs\Julia\Julia-1.4.1\bin
+# Edit existing path: Add new path entry: %JULIA_HOME%
---
+++ useeio-foodwaste/useeio-foodwaste/app/models.py
@@ -0,0 +1,11 @@
+# models.py (app)
+# !/usr/bin/env python3
+# coding=utf-8
+# young.daniel@epa.gov
+
+
+"""Definition of models."""
+
+from django.db import models
+
+# Create your models here.
