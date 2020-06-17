e0e3b82762ba8434c909eab718ddb3948f6ab94d
Jacob Specht <jacob@gqc.com>
2020-06-17 08:58:18 -0400

    Work on notebook from yesterday.

--- useeio-foodwaste/jupyter/lab_data.py
+++ useeio-foodwaste/jupyter/lab_data.py
@@ -21,13 +21,32 @@ import shapefile as shp
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.set(style=”whitegrid”, palette=”pastel”, color_codes=True)
-sns.mpl.rc(“figure”, figsize=(10,6))
+# 3. Initialize visualization set:
+sns.set(style="whitegrid", palette="pastel", color_codes=True)
+sns.mpl.rc("figure", figsize=(10,6))
 
-# Jake, need assistance 'Opening a Vector Map' for Cincinnati, OH.
-# https://catalog.data.gov/dataset/tiger-line-shapefile-2016-state-ohio-current-county-subdivision-state-based
-shp_path = “./jupyter/Vector Maps/tl_2016_39_cousub.shp”
+# 4. Open a vector shape map:
+shp_path = "./VectorMaps/tl_2016_39_cousub.shp"
 sf = shp.Reader(shp_path)
+# Print some of the contents of the shape file:
+# print(len(sf.shapes()))
+# print(sf.records()[1])
+# print(sf.records()[1][5])
+# print(sf.records()[25])
 
-len(sf.shapes())
+# 5. Converting shapefile data on Pandas dataframe
+def read_shapefile(sf):
+    """
+    Read a shapefile into a Pandas dataframe with a 'coords' column
+    holding the geometry information. This uses the pyshp package
+    """
+    fields = [x[0] for x in sf.fields][1:]
+    records = sf.records()
+    shps = [s.points for s in sf.shapes()]
+    df = pd.DataFrame(columns=fields, data=records)
+    df = df.assign(coords=shps)
+    return df
 
+df = read_shapefile(sf)
+# print(df.shape)
+print(df.sample(5))
